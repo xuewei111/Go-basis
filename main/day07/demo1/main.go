3c0fd424cc6b8ad0cfed3a67a996cceef3922556
xuewei111 <m13358109269_1@163.com>
2020-07-13 23:55:30 +0800

    20200713

---
+++ main/day07/demo1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//位运算的演示
+	fmt.Println(2&3)
+
+	fmt.Println(2|3)
+
+	fmt.Println(2^3)
+
+	fmt.Println(-2^2)
+
+}
---
+++ main/day07/demo2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//编写一个程序,可以输入人的年龄,如果该同志的年龄大于18,则输出"大"
+
+
+	var age int8
+	fmt.Println("请输入年龄")
+	fmt.Scanln(&age)
+
+	if age>18 {
+		fmt.Println("大")
+	}
+}
